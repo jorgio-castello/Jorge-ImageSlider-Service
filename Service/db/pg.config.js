a541a9928127cb4033d72ae12df6fa039a489528
Jorge Castello <jorgio.castello@gmail.com>
2020-05-19 15:46:26 -0700

    Incorporate env variables into config

--- Service/db/pg.config.js
+++ Service/db/pg.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   dbConfig: {
-    user: 'postgres',
-    password: 'postgres',
-    host: 'database_maxim',
-    database: 'airbnb_mhyml',
-    port: 5432
+    user: process.env.user || 'postgres',
+    password: process.env.password || 'postgres',
+    host: process.env.host || '127.0.0.1',
+    database: process.env.db || 'airbnb_mhyml',
+    port: process.env.port || 5432
   }
 }
