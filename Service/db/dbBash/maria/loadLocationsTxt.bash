2ab9de65c808da06534b495a966282fdda80cd7c
Jorge Castello <jorgio.castello@gmail.com>
2020-05-22 12:02:32 -0700

    Start preliminary bash scripts for Maria

---
+++ Service/db/dbBash/maria/loadLocationsTxt.bash
@@ -0,0 +1,3 @@
+# RAW MYSQL COMMAND, need to configure as bash script
+
+# LOAD DATA INFILE '/Users/jorgecastello/Code/HackReactor/capstones/SDC/work/ImageService/Service/db/rawData/locationData/MariaCityData.txt' INTO TABLE LOCATION FIELDS TERMINATED BY ',';
