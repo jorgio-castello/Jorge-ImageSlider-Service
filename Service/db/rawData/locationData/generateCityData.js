8413e034850a9e7b8b8f191e710d78de161d0ae5
Jorge Castello <jorgio.castello@gmail.com>
2020-05-22 12:02:05 -0700

    Create generateCityData script for Maria

--- Service/db/rawData/locationData/generateCityData.js
+++ Service/db/rawData/locationData/generateCityData.js
@@ -2,37 +2,33 @@ const faker = require('faker');
 const fs = require('fs');
 const os = require('os');
 
-const numberOfLocations = 2; // Drives the number of locations in this file
-const cityObj = {}; // Temporary save data in an object
+const numberOfLocations = 10000; // Drives the number of locations in this file
+const cityObj = {}; // Temporary save data in an object to prevent duplicate cities
+let cityStrMaria = ''; // This string will include all cities in MySQL ready format
+const mariaTxtFilePath = './MariaCityData.txt';
 
-// Generate Seed Data for Cities and Prevent Duplicates
+// Generate Seed Data for Cities (MARIA TXT FILE FORMAT)
 for (let i = 1; i <= numberOfLocations; i += 1) {
-  cityObj[faker.address.city()] = true;
+  const city = faker.address.city(); // Create a random city
+  if (!cityObj[city]) { // If the city isn't already in the cityObj
+    cityObj[city] = true; // Add it to the cityObj
+    // i represents the primary key, city remends the random city, os.EOL = linebreak
+    cityStrMaria += `${i},${city}${os.EOL}`; // concat the string with this city
+  } else { // if the city has already been added, reset i so we reach 10,000 cities
+    i = i - 1;
+  }
 }
 
 // ----------------------------------------------------------------------------------
 // MARIA - Location Data txt file generation for bulk loading into DB
 // ----------------------------------------------------------------------------------
-const startTimeMaria = new Date(); // Track how long it takes to generateFiles
-const generateTxtDataForMaria = (cityArr, cityEntryCount = 0) => {
-  if (cityEntryCount === cityArr.length) { // BASE CASE: all the cities are added
-    const endTimeMaria = new Date();
-    console.log(`City generation completed in
-                  ${endTimeMaria.getTime() - startTimeMaria.getTime()}ms`
-                );
-  } else {
-    const newCity = cityArr[cityEntryCount];
-    const newCityText = `${cityEntryCount + 1},${newCity}${os.EOL}`;
-
-    fs.appendFile('./MariaCityData.txt', newCityText, (err) => {
-      if (err) {
-        throw new Error(err);
-      }
-      generateTxtDataForMaria(cityArr, cityEntryCount + 1);
-    });
-  }
+const generateTxtDataForMaria = (cityStrMaria) => {
+  fs.appendFile(mariaTxtFilePath, cityStrMaria, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+    console.log('Write complete');
+  });
 };
 
-generateTxtDataForMaria(Object.keys(cityObj));
-
-// let citiesForTxtFile = cityArr.map((city, index) => )
+generateTxtDataForMaria(cityStrMaria);
