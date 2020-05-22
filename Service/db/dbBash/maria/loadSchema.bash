8d4e0f2bf60375b9b9d9d2775bf993319cc7a206
Jorge Castello <jorgio.castello@gmail.com>
2020-05-22 16:25:03 -0700

    Refine Maria bash script

--- Service/db/dbBash/maria/loadSchema.bash
+++ Service/db/dbBash/maria/loadSchema.bash
@@ -1,1 +1,22 @@
-mysql -u root -p < ../../maria/schema.sql
+# VARIABLES --------------------------------------------------------------------------------
+# Maria SQL file
+sqlFile=$(pwd)/db/maria/schema.sql
+
+# # Location Data
+createMariaLocationTxt=$(pwd)/db/rawData/locationData/generateCityData.js
+mariaLocationTxt=$(pwd)/db/rawData/locationData/MariaCityData.txt
+
+# Property Data
+# createMariaPropertyData=$(pwd)/rawData/propertyData/generatePropertyData.js
+# mariaPropertyTxt=$(pwd)/rawData/propertyData/MariaPropertyData.txt
+# ------------------------------------------------------------------------------------------
+# Operations -------------------------------------------------------------------------------
+# # Delete old text files
+rm $mariaLocationTxt || true
+# # rm $mariaPropertyTxt || true
+
+# # Create new text files
+node $createMariaLocationTxt
+
+# Seed database
+mysql -u root -p < $sqlFile
