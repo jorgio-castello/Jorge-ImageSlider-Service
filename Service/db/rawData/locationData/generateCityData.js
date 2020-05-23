53a4497ce23cc9a0f6e593be171eb51028a58859
Jorge Castello <jorgio.castello@gmail.com>
2020-05-22 18:03:21 -0700

    Refactor generateCityData to use helper functions

--- Service/db/rawData/locationData/generateCityData.js
+++ Service/db/rawData/locationData/generateCityData.js
@@ -1,24 +1,11 @@
-const faker = require('faker');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
-const numberOfLocations = 10000; // Drives the number of locations in this file
-const cityObj = {}; // Temporary save data in an object to prevent duplicate cities
-let cityStrMaria = ''; // This string will include all cities in MySQL ready format
-const mariaTxtFilePath = path.join(__dirname, 'MariaCityData.txt');
+const { createMariaLocationString } = require('./helpers/createMariaLocationString');
 
-// Generate Seed Data for Cities (MARIA TXT FILE FORMAT)
-for (let i = 1; i <= numberOfLocations; i += 1) {
-  const city = faker.address.city(); // Create a random city
-  if (!cityObj[city]) { // If the city isn't already in the cityObj
-    cityObj[city] = true; // Add it to the cityObj
-    // i represents the primary key, city remends the random city, os.EOL = linebreak
-    cityStrMaria += `${i},${city}${os.EOL}`; // concat the string with this city
-  } else { // if the city has already been added, reset i so we reach 10,000 cities
-    i = i - 1;
-  }
-}
+const numberOfLocations = 10000; // Drives the number of locations in this file
+const mariaTxtFilePath = path.join(__dirname, 'MariaCityData.txt');
 
 // ----------------------------------------------------------------------------------
 // MARIA - Location Data txt file generation for bulk loading into DB
@@ -36,8 +23,5 @@ const generateTxtDataForMaria = (cityStrMaria) => {
   });
 };
 
+const cityStrMaria = createMariaLocationString(numberOfLocations);
 generateTxtDataForMaria(cityStrMaria);
-
-module.exports.cityStrMaria = cityStrMaria;
-module.exports.generateTxtDataForMaria = generateTxtDataForMaria;
-module.exports.numberOfLocations = numberOfLocations;
