cce0ab16b0fa356650031ad870a8066391f5d480
Jorge Castello <jorgio.castello@gmail.com>
2020-05-24 09:51:48 -0700

    Adjust property logic to account for large strings

--- Service/db/rawData/propertyData/generatePropertyData.js
+++ Service/db/rawData/propertyData/generatePropertyData.js
@@ -1,31 +1,64 @@
 const faker = require('faker');
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 
 const numberOfProperties = 10000000;
 
 const mariaPropertyDataPath = path.join(__dirname, './MariaPropertyData.txt');
+const mariaPropertyPhotoUrls = path.join(__dirname, '../photoData/S3-picture-urls.txt');
 
 const { createMariaPropertyStr } = require('./helpers/createMariaPropertyStr');
 
 const writeMariaPropertyData = () => {
   let propertyMariaStr = '';
-  for (let i = 1; i <= numberOfProperties; i++) {
-    propertyMariaStr += createMariaPropertyStr(i);
-  }
+  const propertyMariaStringsArr = [];
 
-  const startTime = new Date();
-  console.log(`Starting to write to txt file called ${path.basename(mariaPropertyDataPath)}`);
-
-  fs.appendFile(mariaPropertyDataPath, propertyMariaStr, (err) => {
+  fs.readFile(mariaPropertyPhotoUrls, 'utf-8', (err, photoBlocksStr) => {
     if (err) {
       throw new Error(err);
     } else {
+      const blockUrls = photoBlocksStr.split(os.EOL);
+
+      console.log('Working on Property #1');
+      for (let i = 1; i <= numberOfProperties; i++) {
+        if (propertyMariaStr.length > 2 ** 28 - 1) {
+          propertyMariaStringsArr.push(propertyMariaStr);
+          propertyMariaStr = '';
+          console.log(`Working on Property #${i}`);
+        }
+        propertyMariaStr += createMariaPropertyStr(i, faker.random.arrayElement(blockUrls));
+      }
+
+      if (propertyMariaStr.length) {
+        propertyMariaStringsArr.push(propertyMariaStr);
+      }
+
+
+      const createMariaPropertyTxtFile = (count = 0) => {
+        if (count !== propertyMariaStringsArr.length) {
+          fs.appendFile(mariaPropertyDataPath, propertyMariaStringsArr[count], (err) => {
+            if (err) {
+              throw new Error(err);
+            } else {
+              createMariaPropertyTxtFile(count + 1);
+            }
+          });
+        }
+      }
+      const startTime = new Date();
+      console.log(`Starting to write to txt file called ${path.basename(mariaPropertyDataPath)}`);
+      createMariaPropertyTxtFile();
       const endTime = new Date();
-      const time = startTime.getTime() / 1000 - endTime.getTime() / 1000;
+      const time = endTime.getTime() / 1000 - startTime.getTime() / 1000;
       console.log(`Finished in ${time} seconds`);
     }
   });
+
+
+
+
+
 };
 
 writeMariaPropertyData();
