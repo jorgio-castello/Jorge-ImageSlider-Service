97fed47a3b0f1631fc09647b29e87438b41c3f8f
Jorge Castello <jorgio.castello@gmail.com>
2020-05-23 18:23:10 -0700

    Accept photoUrl in the property data helper func

--- Service/db/rawData/propertyData/helpers/createMariaPropertyStr.js
+++ Service/db/rawData/propertyData/helpers/createMariaPropertyStr.js
@@ -10,13 +10,13 @@ const { generatePropertyType,   // generates a random property
 // Based on the number of locations created in locationsData file
 const numberOfLocations = 10000;
 
-const createMariaPropertyStr = (primaryKey) => {
+const createMariaPropertyStr = (primaryKey, photoUrl) => {
   const rating = generateRandomNumber(100, 500) / 100;
   const property_type = generatePropertyType();
   const bed_num = numberOfBeds[property_type]();
   const description = generateDescription();
   const price_per_night = bed_num * priceByPropertyPerRoom[property_type]();
-  const awsBlockUrl = 'url';
+  const awsBlockUrl = photoUrl;
   const location_id = generateRandomNumber(1, numberOfLocations);
 
   return `${primaryKey},${rating},'${property_type}',${bed_num},'${description}',${price_per_night},'${awsBlockUrl}',${location_id}${os.EOL}`;
