b275fb0e1141210797f13193c6f33686fa7a53db
Jorge Castello <jorgio.castello@gmail.com>
2020-05-24 22:53:05 -0700

    Update propertyGenerator for Cassandra

--- Service/db/rawData/propertyData/generatePropertyData.js
+++ Service/db/rawData/propertyData/generatePropertyData.js
@@ -1,4 +1,3 @@
-const faker = require('faker');
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
@@ -6,34 +5,18 @@ const path = require('path');
 const numberOfProperties = 10000000;
 
 const mariaPropertyDataPath = path.join(__dirname, './MariaPropertyData.txt');
-const mariaPropertyPhotoUrls = path.join(__dirname, '../photoData/S3-picture-urls.txt');
+const cassandraPropertyDataPath = path.join(__dirname, './CassandraPropertyData.csv');
+const photoBlockUrls = path.join(__dirname, '../photoData/S3-picture-urls.txt');
 
-const { createMariaPropertyStr } = require('./helpers/createMariaPropertyStr');
+const { createCassandraPropertyData, createMariaPropertyData } = require('./helpers/createPropertyData');
 
 const writeMariaPropertyData = () => {
-  let propertyMariaStr = '';
-  const propertyMariaStringsArr = [];
-
-  fs.readFile(mariaPropertyPhotoUrls, 'utf-8', (err, photoBlocksStr) => {
+  fs.readFile(photoBlockUrls, 'utf-8', (err, photoBlocksStr) => {
     if (err) {
       throw new Error(err);
     } else {
       const blockUrls = photoBlocksStr.split(os.EOL);
-
-      console.log('Working on Property #1');
-      for (let i = 1; i <= numberOfProperties; i++) {
-        if (propertyMariaStr.length > 2 ** 28 - 1) {
-          propertyMariaStringsArr.push(propertyMariaStr);
-          propertyMariaStr = '';
-          console.log(`Working on Property #${i}`);
-        }
-        propertyMariaStr += createMariaPropertyStr(i, faker.random.arrayElement(blockUrls));
-      }
-
-      if (propertyMariaStr.length) {
-        propertyMariaStringsArr.push(propertyMariaStr);
-      }
-
+      const propertyMariaStringsArr = createMariaPropertyData(blockUrls, numberOfProperties);
 
       const createMariaPropertyTxtFile = (count = 0) => {
         if (count !== propertyMariaStringsArr.length) {
@@ -46,6 +29,7 @@ const writeMariaPropertyData = () => {
           });
         }
       }
+
       const startTime = new Date();
       console.log(`Starting to write to txt file called ${path.basename(mariaPropertyDataPath)}`);
       createMariaPropertyTxtFile();
@@ -54,13 +38,36 @@ const writeMariaPropertyData = () => {
       console.log(`Finished in ${time} seconds`);
     }
   });
-
-
-
-
-
 };
 
-writeMariaPropertyData();
+const writeCassandraPropertyData = () => {
+  fs.readFile(photoBlockUrls, 'utf-8', (err, photoBlocksStr) => {
+    if (err) {
+      throw new Error(err);
+    } else {
+      const blockUrls = photoBlocksStr.split(os.EOL);
+      const propertyCassandraStringsArr = createCassandraPropertyData(blockUrls, numberOfProperties, (propertyCassandraStringsArr) => {
+        const createCassandraPropertyCSVFile = (count = 0) => {
+          if (count !== propertyCassandraStringsArr.length) {
+            fs.appendFile(cassandraPropertyDataPath, propertyCassandraStringsArr[count], (err) => {
+              if (err) {
+                throw new Error(err);
+              } else {
+                createCassandraPropertyCSVFile(count + 1);
+              }
+            });
+          }
+        }
+        const startTime = new Date();
+        console.log(`Starting to write to txt file called ${path.basename(cassandraPropertyDataPath)}`);
+        createCassandraPropertyCSVFile();
+        const endTime = new Date();
+        const time = endTime.getTime() / 1000 - startTime.getTime() / 1000;
+        console.log(`Finished in ${time} seconds`);
+      });
+    }
+  });
+}
 
-// Need to read in photo data, pass it to createMariaPropertyStr, and figure out a way to get that into .txt file
+// writeMariaPropertyData();
+writeCassandraPropertyData();
