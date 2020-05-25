a5b8497561462bf5a4705d5166d6edf3e525a600
Jorge Castello <jorgio.castello@gmail.com>
2020-05-24 22:52:22 -0700

    Update bash script for Cassandra (property data)

--- Service/db/dbBash/cassandra/createDataAndSeedDB.bash
+++ Service/db/dbBash/cassandra/createDataAndSeedDB.bash
@@ -7,17 +7,17 @@ createLocationCSV=$(pwd)/db/rawData/locationData/generateCityData.js
 cassandraLocationCSV=$(pwd)/db/rawData/locationData/CassandraCityData.csv
 
 # # Property Data
-# createMariaPropertyData=$(pwd)/db/rawData/propertyData/generatePropertyData.js
-# mariaPropertyTxt=$(pwd)/db/rawData/propertyData/MariaPropertyData.txt
+createPropertyData=$(pwd)/db/rawData/propertyData/generatePropertyData.js
+cassandraPropertyCSV=$(pwd)/db/rawData/propertyData/CassandraPropertyData.csv
 # ------------------------------------------------------------------------------------------
 # Operations -------------------------------------------------------------------------------
 # # Delete old text files
 rm $cassandraLocationCSV || true
-# rm $mariaPropertyTxt || true
+rm $cassandraPropertyCSV || true
 
 # # Create new text files
 node $createLocationCSV
-# node --max-old-space-size=20000 $createMariaPropertyData
+node --max-old-space-size=20000 $createPropertyData
 
 # Seed database
 cqlsh 127.0.0.1 9042 source < $cqlFile;
