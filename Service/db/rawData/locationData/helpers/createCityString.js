784ac51da29ce3c834f5bf19e5a889991f07836a
Jorge Castello <jorgio.castello@gmail.com>
2020-05-24 18:09:48 -0700

    Rename cityGenerator helper func

--- Service/db/rawData/locationData/helpers/createCityString.js
+++ Service/db/rawData/locationData/helpers/createCityString.js
@@ -1,9 +1,9 @@
 const faker = require('faker');
 const os = require('os');
 
-const createMariaLocationString = (numberOfLocations) => {
+const createCityString = (numberOfLocations) => {
   const cityObj = {}; // Temporary save data in an object to prevent duplicate cities
-  let cityStrMaria = ''; // This string will include all cities in Maria ready format
+  let cityStr = ''; // This string will include all cities in Maria ready format
 
   // Generate Seed Data for Cities (MARIA TXT FILE FORMAT)
   for (let i = 1; i <= numberOfLocations; i += 1) {
@@ -11,13 +11,13 @@ const createMariaLocationString = (numberOfLocations) => {
     if (!cityObj[city]) { // If the city isn't already in the cityObj
       cityObj[city] = true; // Add it to the cityObj
       // i represents the primary key, city remends the random city, os.EOL = linebreak
-      cityStrMaria += `${i},${city}${os.EOL}`; // concat the string with this city
+      cityStr += `${i},${city}${os.EOL}`; // concat the string with this city
     } else { // if the city has already been added, reset i so we reach 10,000 cities
       i = i - 1;
     }
   }
 
-  return cityStrMaria;
+  return cityStr;
 }
 
-module.exports.createMariaLocationString = createMariaLocationString;
+module.exports.createCityString = createCityString;
