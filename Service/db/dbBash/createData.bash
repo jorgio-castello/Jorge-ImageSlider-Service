e8b5eff3f4767f25cb5bf23fb6d1d6d9ec29f9b8
Jorge Castello <jorgio.castello@gmail.com>
2020-05-26 11:22:36 -0700

    Tweak bash scripts

---
+++ Service/db/dbBash/createData.bash
@@ -0,0 +1,33 @@
+# VARIABLES --------------------------------------------------------------------------------
+# City / Location Data - Generation Scripts
+createLocationsMaria=$(pwd)/db/seedScripts/createMariaCityData.js
+createPropertiesMaria=$(pwd)/db/seedScripts/createMariaPropertyData.js
+createLocationsCassandra=$(pwd)/db/seedScripts/createCassandraCityData.js
+createPropertiesCassandra=$(pwd)/db/seedScripts/createCassandraPropertyData.js
+
+# Data file locations ----------------------------------------------------------------------
+mariaLocationTxt=$(pwd)/db/rawData/locationData/MariaCityData.txt
+mariaPropertyTxt=$(pwd)/db/rawData/propertyData/MariaPropertyData.txt
+cassandraLocationCSV=$(pwd)/db/rawData/locationData/CassandraCityData.csv
+cassandraPropertyCSV=$(pwd)/db/rawData/propertyData/CassandraPropertyData.csv
+cassandraPropertyByPriceCSV=$(pwd)/db/rawData/propertyData/CassandraPropertyDataByPrice.csv
+cassandraPropertyByBedsCSV=$(pwd)/db/rawData/propertyData/CassandraPropertyDataByBeds.csv
+
+# OPERATIONS -------------------------------------------------------------------------------
+# Delete old files -------------------------------------------------------------------------
+rm $mariaLocationTxt || true
+rm $mariaPropertyTxt || true
+rm $cassandraLocationCSV || true
+rm $cassandraPropertyCSV || true
+rm $cassandraPropertyByPriceCSV || true
+rm $cassandraPropertyByBedsCSV || true
+
+# Create new data files --------------------------------------------------------------------
+echo "GENERATING MARIA LOCATIONS DATA..."
+# node --max-old-space-size=20000 $createLocationsMaria
+echo "GENERATING MARIA PROPERTIES DATA..."
+# node --max-old-space-size=20000 $createPropertiesMaria
+echo "GENERATING CASSANDRA LOCATIONS DATA..."
+# node --max-old-space-size=20000 $createLocationsCassandra
+echo "GENERATING CASSANDRA PROPERTIES DATA..."
+# node --max-old-space-size=20000 $createPropertiesCassandra
