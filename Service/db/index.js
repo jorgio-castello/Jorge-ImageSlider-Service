d1e81871891d99c8a0da5ea7427731a11d7e4109
Jorge Castello <jorgio.castello@gmail.com>
2020-05-26 08:33:45 -0700

    Remove db files from prior service

--- Service/db/index.js
+++
@@ -1,9 +0,0 @@
-// use 'npm pg' documentation
-const { Client } = require('pg');
-const { dbConfig } = require('./pg.config.js');
-
-const client = new Client(dbConfig);
-
-client.connect();
-
-module.exports = client;
--- Service/db/pg.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dbConfig: {
-    user: process.env.user || 'postgres',
-    password: process.env.password || 'postgres',
-    host: process.env.host || '127.0.0.1',
-    database: process.env.db || 'airbnb_mhyml',
-    port: process.env.port || 5432
-  }
-}
