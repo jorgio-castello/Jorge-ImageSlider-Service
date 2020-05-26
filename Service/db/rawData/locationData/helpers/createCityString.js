bf164b0b154a5775cbb7e362d9eaec16b143efc6
Jorge Castello <jorgio.castello@gmail.com>
2020-05-26 01:39:54 -0700

    Create city string helper func for Cassandra

--- Service/db/rawData/locationData/helpers/createCityString.js
+++ Service/db/rawData/locationData/helpers/createCityString.js
@@ -1,7 +1,16 @@
 const faker = require('faker');
+const fs = require('fs');
 const os = require('os');
+const path = require('path');
 
-const createCityString = (numberOfLocations) => {
+const { generatePropertyType,   // generates a random property
+  numberOfBeds,           // generates a random number of beds based on property type
+  priceByPropertyPerRoom, // generates a random price based on property type
+  generateRandomNumber    // generates a random number between a given min and max
+} = require('../../propertyData/helpers/index');
+
+
+const createCityStringMaria = (numberOfLocations) => {
   const cityObj = {}; // Temporary save data in an object to prevent duplicate cities
   let cityStr = ''; // This string will include all cities in Maria ready format
 
@@ -18,6 +27,31 @@ const createCityString = (numberOfLocations) => {
   }
 
   return cityStr;
-}
+};
 
-module.exports.createCityString = createCityString;
+const createCityStringCassandra = (numberOfLocations, callback) => {
+  fs.readFile(path.join(__dirname,'../MariaCityData.txt'), 'utf-8', (err, data) => {
+    const cities = data.split(os.EOL).map(city => city.split(',')).map(city => city[1]);
+    let cassandraCityStr = '';
+    let cassandraCityStrArr = [];
+
+    for (let i = 1; i <= numberOfLocations; i += 1) {
+      if (cassandraCityStr.length > 2 ** 28 - 1) {
+        cassandraCityStrArr.push(cassandraCityStr);
+        cassandraCityStr = '';
+      }
+      let propertyType = generatePropertyType();
+      let bedNum = numberOfBeds[propertyType]();
+      cassandraCityStr += `${i},${bedNum},${faker.random.arrayElement(cities)},${bedNum * priceByPropertyPerRoom[propertyType]()},${propertyType},${generateRandomNumber(100, 500) / 100}${os.EOL}`;
+    }
+
+    if ( cassandraCityStr.length ) {
+      cassandraCityStrArr.push(cassandraCityStr);
+    }
+
+    callback(cassandraCityStrArr);
+  });
+};
+
+module.exports.createCityStringMaria = createCityStringMaria;
+module.exports.createCityStringCassandra = createCityStringCassandra;
