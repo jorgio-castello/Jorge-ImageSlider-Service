fccf0632a834dba198825b3856a13456fd092757
Jorge Castello <jorgio.castello@gmail.com>
2020-05-30 10:59:52 -0700

    Implement delete models using prepared statements

--- Service/db/cassandra/models/handleDeleteRequest.js
+++ Service/db/cassandra/models/handleDeleteRequest.js
@@ -1,3 +1,19 @@
 const db = require('..');
+const { assembleDeleteRequest } = require('./helpers/deleteRequestHelpers');
 
-const deleteLocationHandler
+const handleDeleteRequest = (tables, tableToCharacteristic, data, id, callback) => {
+  const { location, uuid } = data;
+
+  const queries = tables.map(table => assembleDeleteRequest(table, location, uuid, data[tableToCharacteristic[table]]));
+
+  queries.push({
+    query: 'delete from locations where address_id=?',
+    params: [id],
+  });
+
+  db.batch(queries, { prepare: true })
+    .then((success) => callback(null, success))
+    .catch((err) => callback(err));
+};
+
+module.exports.handleDeleteRequest = handleDeleteRequest;
