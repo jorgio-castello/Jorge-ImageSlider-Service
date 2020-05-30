4157871300e8c99e1acdea5298c62a83c4d46585
Jorge Castello <jorgio.castello@gmail.com>
2020-05-29 19:33:45 -0700

    Update server index for appropriate get route

--- Service/server/index.js
+++ Service/server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { getRoomData, createHost, createRoom, updateHost, updateRoom, deleteHost, deleteRoomsFromHost } = require('./routeHandlers.js');
+const cache = require('express-cache-controller');
+
+// Controllers
+const { getSimilarHomes } = require('./controllers/getSimilarHomes');
 
 const app = express();
 app.use(express.json());
@@ -7,18 +10,12 @@ app.use(express.json());
 const PORT = 3004;
 app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
 
+app.use(cache({maxAge: 31536000}));
 app.use(express.static('public'));
-app.get('*.js', function (req, res, next) {
+app.get('*.js', (req, res, next) => {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
   next();
 });
 
-// Get Route - currently this is sending back the whole DB, will refactor schema to include a property table
-app.get('/rooms', getRoomData);
-app.post('/createHost', createHost); // Post Route - Host
-app.post('/createRoom', createRoom); // Post Route - Rooms
-app.put('/updateHost', updateHost);  // Put Route - Host
-app.put('/updateRoom', updateRoom);  // Put Route - Rooms
-app.delete('/deleteHost', deleteHost); // Delete Route - Host
-app.delete('/deleteRoomsFromHost', deleteRoomsFromHost); // Delete an item
+app.get('/properties/:id/similarHomes/:rankBy?/:limit?', getSimilarHomes);
