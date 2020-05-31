162af691e20bebbc2019724f1e91c4fad76ab76d
Jorge Castello <jorgio.castello@gmail.com>
2020-05-31 12:51:05 -0700

    Update Cassandra connection to AWS

--- Service/db/cassandra/index.js
+++ Service/db/cassandra/index.js
@@ -1,7 +1,7 @@
 const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({
-  contactPoints: ['127.0.0.1'],
+  contactPoints: ['0.0.0.0'],
   keyspace: 'similar_properties',
   localDataCenter: 'datacenter1',
 });
