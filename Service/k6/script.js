3cdd539770ce902931634cd082dc6ca5768b6fd0
Jorge Castello <jorgio.castello@gmail.com>
2020-05-31 12:51:05 -0700

    Saving work for clustering

--- Service/k6/script.js
+++ Service/k6/script.js
@@ -3,23 +3,23 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '1m', target: 300 }, // below normal load
-    { duration: '1m', target: 500 },
-    { duration: '1m', target: 700 }, // normal load
-    { duration: '1m', target: 900 },
-    { duration: '1m', target: 1100 }, // around the breaking point
-    { duration: '1m', target: 1200 },
-    { duration: '1m', target: 1300 }, // beyond the breaking point
-    { duration: '1m', target: 1400 },
-    { duration: '1m', target: 1000 }, // below normal load
-    { duration: '1m', target: 800 }, // around the breaking point
-    { duration: '1m', target: 400 },
-    { duration: '1m', target: 0 }, // scale down. Recovery stage.
+    { duration: '2m', target: 2200 },
+    { duration: '2m', target: 2400 },
+    { duration: '2m', target: 2700 },
+    { duration: '2m', target: 3000 },
+    { duration: '2m', target: 3500 },
+    { duration: '2m', target: 4000 },
+    { duration: '2m', target: 4500 },
+    { duration: '2m', target: 3000 },
+    { duration: '2m', target: 1900 },
+    { duration: '2m', target: 1200 },
+    { duration: '2m', target: 800 },
+    { duration: '2m', target: 0 },
   ],
 };
 
 export default function() {
   const id = Math.ceil(Math.random() * 10000000);
   http.get(`http://localhost:3004?id=${id}`);
-  sleep(.75);
+  sleep(.5);
 }
--- Service/server/index.js
+++ Service/server/index.js
@@ -1,31 +1,43 @@
-require('newrelic');
-const express = require('express');
-const cache = require('express-cache-controller');
+const os = require('os');
+const cluster = require('cluster');
 
-// Controllers
-const { getSimilarHomes } = require('./controllers/getSimilarHomes');
-const { postNewProperty } = require('./controllers/postNewProperty');
-// const { patchProperty } = require('./controllers/patchProperty');
-const { deleteProperty } = require('./controllers/deleteProperty');
+if (cluster.isMaster) {
+  const n_cpus = os.cpus().length;
+  console.log(`forking ${n_cpus} CPUs`);
+  for (let i = 0; i < n_cpus; i += 1) {
+    cluster.fork();
+  }
+} else {
+  require('newrelic');
+  const express = require('express');
+  const cache = require('express-cache-controller');
 
-const app = express();
-app.use(express.json());
+  // Controllers
+  const { getSimilarHomes } = require('./controllers/getSimilarHomes');
+  const { postNewProperty } = require('./controllers/postNewProperty');
+  // const { patchProperty } = require('./controllers/patchProperty');
+  const { deleteProperty } = require('./controllers/deleteProperty');
 
-const PORT = 3004;
-app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
+  const app = express();
+  app.use(express.json());
 
-app.use(cache({maxAge: 31536000}));
-app.use(express.static('public'));
-app.get('*.js', (req, res, next) => {
-  req.url += '.gz';
-  res.set('Content-Encoding', 'gzip');
-  next();
-});
+  const PORT = 3004;
+  app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`); });
 
-app.get('/properties/:id/similarHomes/:rankBy?/:limit?', getSimilarHomes);
-app.post('/properties/createProperty', postNewProperty);
-// app.patch('/properties/:id/similarHomes', patchProperty);
-app.delete('/properties/:id/similarHomes', deleteProperty);
+  app.use(cache({maxAge: 31536000}));
+  app.use(express.static('public'));
+  app.get('*.js', (req, res, next) => {
+    req.url += '.gz';
+    res.set('Content-Encoding', 'gzip');
+    next();
+  });
+
+  app.get('/properties/:id/similarHomes/:rankBy?/:limit?', getSimilarHomes);
+  app.post('/properties/createProperty', postNewProperty);
+  // app.patch('/properties/:id/similarHomes', patchProperty);
+  app.delete('/properties/:id/similarHomes', deleteProperty);
+}
+
 
 // Will need to update locations schema in order to properly create PATCH Request
 // The location schema needs to include all relevant data about the property, so that a successful patch request can bee created, need to awsBlockUrl, and description
