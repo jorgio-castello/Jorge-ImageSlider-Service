fd55ebe3b4a8b9ae16617d62497cbd09dff003c0
Jorge Castello <jorgio.castello@gmail.com>
2020-06-17 20:55:21 -0700

    Introduce env variables for Redis connection

--- Service/server/controllers/getSimilarHomes.js
+++ Service/server/controllers/getSimilarHomes.js
@@ -3,7 +3,7 @@ const { handleLocationQuery } = require('../../db/cassandra/models/handleGetRequ
 // Helpers
 const { reassignParamsIfEmpty, handleSpecificPropertyQuery, handleGeneralPropertyQuery } = require('./helpers/getSimilarHomesHelpers');
 
-const redisUrl = '34.220.94.109';
+const redisUrl = process.env.redisURL || '127.0.0.1';
 const redisPort = 6379;
 
 const redisClient = require('redis').createClient;
@@ -30,10 +30,10 @@ const getSimilarHomes = (req, res) => {
     } else {
       redis.get(uuid, (redisErr, results) => {
         if (redisErr) {
-	 throw new Error(redisErr);
-	} else if (results) {
+	        throw new Error(redisErr);
+	      } else if (results) {
           res.json(results);
-	} else {
+	      } else {
           handleGeneralPropertyQuery(locationQueryData, location, 3, (propertyErr, propertyData) => {
              if (propertyErr) {
                 throw new Error(propertyErr);
