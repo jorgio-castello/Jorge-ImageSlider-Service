bff140ecd8fc02f9d549adbee48c82fb7dc00557
Jorge Castello <jorgio.castello@gmail.com>
2020-06-17 20:55:51 -0700

    Introduce Redis envvar / introduce default limit

--- Service/server/controllers/helpers/getSimilarHomesHelpers.js
+++ Service/server/controllers/helpers/getSimilarHomesHelpers.js
@@ -1,7 +1,7 @@
 const { tableToCharacteristicMap, tableToCharacteristicArr } = require('./tableGlossary');
 const { handlePropertyQuery } = require('../../../db/cassandra/models/handleGetRequest');
 
-const redisUrl = '34.220.94.109';
+const redisUrl = process.env.redisURL || '127.0.0.1';
 const redisPort = 6379;
 
 const redisClient = require('redis').createClient;
@@ -39,6 +39,10 @@ const handleGeneralPropertyQuery = (data, location, limit, callback) => {
 const reassignParamsIfEmpty = ({ id, rankBy, limit }) => {
   if (id === 'null') {
     return [Math.floor(Math.random(0, 1) * 10000000)];
+  }
+
+  if (!limit) {
+    limit = 1;
   }
 
   return [id, rankBy, limit];
